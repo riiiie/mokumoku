88382775f5874ba1763ca87f671e07716972c73c
riiiie <rieohya008@gmail.com>
2023-05-22 22:33:43 +0900

    relationships_controller.rb の user=User.find(params[:user_id]) を共通化しました

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,8 @@
 class RelationshipsController < ApplicationController
+
+  before_action :set_user, only: %i[create followings followers]
   # フォローするとき
   def create
-    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
@@ -10,14 +11,18 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(params[:user_id])
     redirect_to request.referer  
   end
-  # フォロー一覧
+  # フォロー一覧（今回は使わない）
   def followings
-    user = User.find(params[:user_id])
     @users = user.followings
   end
-  # フォロワー一覧
+  # フォロワー一覧（今回は使わない）
   def followers
-    user = User.find(params[:user_id])
     @users = user.followers
+  end
+
+  private
+
+  def set_user
+    user = User.find(params[:user_id])
   end
 end
